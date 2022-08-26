b1a48d53618176a93835065f73d320c8596b5b44
fluffy <fluffyyyyyyyy>
2022-08-26 12:07:48 -0400

    Working. Known Issues: - Ignores weight for stacking to tiles - Doesn't update player weight until next turn.

--- src/SmartStack.ts
+++ src/SmartStack.ts
@@ -1,8 +1,7 @@
-import Doodad from "game/doodad/Doodad";
-import { Action } from "game/entity/action/Action";
-import { EntityType } from "game/entity/IEntity";
-import { ContainerReferenceType } from "game/item/IItem";
-
+// import Doodad from "game/doodad/Doodad";
+// import { Action } from "game/entity/action/Action";
+// import { EntityType } from "game/entity/IEntity";
+// import { ContainerReferenceType } from "game/item/IItem";
 import Mod from "mod/Mod";
 import Register, { Registry } from "mod/ModRegistry";
 import { ActionType } from "game/entity/action/IAction"
@@ -11,8 +10,12 @@ import { IInput } from "ui/input/IInput"
 import Message from "language/dictionary/Message";
 import Log from "utilities/Log";
 import Bind from "ui/input/Bind";
-//import { StackNearby } from "./StackNearby";
+import { StackNearby } from "./StackNearby";
 import ActionExecutor from "game/entity/action/ActionExecutor";
+// import { EventHandler } from "event/EventManager";
+// import { EventBus } from "event/EventBuses";
+// import { Game } from "game/Game";
+
 
 export default class SmartStack extends Mod {
     @Mod.instance<SmartStack>("SmartStack")
@@ -21,98 +24,38 @@ export default class SmartStack extends Mod {
     @Mod.log()
     public static readonly LOG: Log;
 
-    public override onLoad(): void {
-        this.getLog
-    }
     //////////////////////////////////////////////////////////////////////////////////////////////
     // Messages 
     //
-    @Register.message("Search") // Source has no items that match those in the target.
+    @Register.message("Search") // Smart-stack initiated 
     public readonly messageSearch: Message;
 
-    @Register.message("NoMatch") // Source has no items that match those in the target.
+    @Register.message("NoMatch") // No items in inventory match available targets.
     public readonly messageNoMatch: Message;
-
-    @Register.message("StackedNone") // Target too full to deposit anything.
+    
+    @Register.message("StackedNone") // Target found but too full to deposit anything.
     public readonly messageStackedNone: Message;
 
-    @Register.message("StackedSome") // Some matching items deposited to target, but it filled up.
+    @Register.message("StackedSome") // Some matching items deposited to a target, but it filled up.
     public readonly messageStackedSome: Message;
 
-    @Register.message("StackedAll") // All matching items deposited to target.
+    @Register.message("StackedAll") // All matching items deposited to a target.
     public readonly messageStackedAll: Message;
 
     //////////////////////////////////////////////////////////////////////////////////////////////
-    // Bindable
-    //
+    // Actions
+    @Register.action("StackNearby", StackNearby)
+    public readonly actionStackNearby: ActionType;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // Binds
     @Register.bindable("StackNearby", IInput.key("slash", "Shift"))
     public readonly bindableStackNearby: Bindable;
 
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    // Actions and handlers
-    //
-
-    //@Register.action("StackNearby", StackNearby)
-
-    static StackNearby = new Action()
-        .setUsableBy(EntityType.Player)
-        .setHandler((action) => {
-            const player = action.executor;
-            const IM = player.island.items;
-            const validContainerTypes = [ContainerReferenceType.Doodad, ContainerReferenceType.Tile];
-            const containers = IM.getAdjacentContainers(player, false)
-                .filter((c) => validContainerTypes.includes(IM.getContainerReference(c, undefined).crt))
-                .filter((c) => c.containedItems.length > 0);
-
-            const invFreeItems = player.inventory.containedItems.filter(it => (!it.isEquipped() && !it.isProtected())); // Exclude protected and equipped.
-            const invTypes = invFreeItems.map(it => it.type);
-            const invTypesUnique = invTypes.filter((ty, i) => invTypes.indexOf(ty) === i);
-
-            let haveMatched = false;
-            if (containers.length) {
-                SmartStack.LOG.info(`Sending message 'messageSearch'...`);
-                player.messages.send(Registry<SmartStack>().get("messageSearch"));
-            }
-            containers.forEach((c) => {
-                let cRef = IM.getContainerReference(c, undefined);
-                let tgtStr: string;
-
-                if (cRef.crt == ContainerReferenceType.Doodad) {
-                    tgtStr = "into " + (c as Doodad).getName().getString();
-                } else {
-                    tgtStr = "onto the ground";
-                }
-
-                let cTypes = c.containedItems.map(it => it.type);
-                let cTypesUniqueMatched = cTypes.filter((ty, i) => cTypes.indexOf(ty) === i)
-                    .filter(ty => invTypesUnique.includes(ty));
-
-                cTypesUniqueMatched.forEach((type) => {
-                    let nHave = player.inventory.containedItems.filter(it => it.type == type).length;
-                    let nMoved = IM.moveAllFromContainerToContainer(player, player.inventory, c, type, undefined, true).length;
-                    let itemStr = `${nMoved} ` + IM.getItemTypeGroupName(type, false, nMoved > 0 ? nMoved : 5).getString();
-                    haveMatched = true;
-                    let msgKey: keyof SmartStack;
-                    if (nMoved == nHave) msgKey = "messageStackedAll";
-                    else if (nMoved > 0) msgKey = "messageStackedSome";
-                    else msgKey = "messageStackedNone";
-                    SmartStack.LOG.info(`Sending message '${msgKey}'...`);
-                    player.messages.send(Registry<SmartStack>().get(msgKey), itemStr, tgtStr);
-                });
-            });
-            if (!haveMatched) player.messages.send(Registry<SmartStack>().get("messageNoMatch"));
-        });
-    @Register.action("StackNearby", SmartStack.StackNearby)
-    public readonly actionStackNearby: ActionType;
-
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    // Bind
-    //
     @Bind.onDown(Registry<SmartStack>().get("bindableStackNearby"))
     public Activate() {
         SmartStack.LOG.info("Received keybind!");
-        ActionExecutor.get(SmartStack.StackNearby).execute(localPlayer);
+        ActionExecutor.get(StackNearby).execute(localPlayer);
         return true;
     };
 }
--- src/StackNearby.ts
+++ src/StackNearby.ts
@@ -1,55 +1,80 @@
 import Doodad from "game/doodad/Doodad";
 import { Action } from "game/entity/action/Action";
 import { EntityType } from "game/entity/IEntity";
-import { ContainerReferenceType } from "game/item/IItem";
-import { Registry } from "mod/ModRegistry";
+import { ContainerReferenceType, ItemType } from "game/item/IItem";
 import SmartStack from "./SmartStack";
+import { IActionHandlerApi } from "game/entity/action/IAction";
+import Player from "game/entity/player/Player";
+import { IActionUsable } from "game/entity/action/IAction";
+import Item from "game/item/Item";
+
+import { TurnType } from "game/entity/player/IPlayer";
+
+export function handleStackNearby(action: IActionHandlerApi<Player, IActionUsable>): void {
+    const player = action.executor;
+    const IMgr = player.island.items;
+    const validContainerTypes = [ContainerReferenceType.Doodad, ContainerReferenceType.Tile];
+    const containers = IMgr.getAdjacentContainers(player, false)
+        .filter((c) => validContainerTypes.includes(IMgr.getContainerReference(c, undefined).crt))
+        .filter((c) => c.containedItems.length > 0);
+
+    let haveMatched = false;
+    let haveMoved = false;
+    let invChanged: boolean = true;
+    let invFreeItems: Item[];
+    let invTypes: ItemType[];
+    let invTypesUnique: ItemType[];
+
+    if(containers.length) player.messages.send(SmartStack.INSTANCE.messageSearch);
+    containers.forEach((c) => {
+        let cRef = IMgr.getContainerReference(c, undefined);
+        let tgtStr: string;
+        // let isTile: boolean;
+        if (cRef.crt == ContainerReferenceType.Doodad) {
+            tgtStr = "into " + (c as Doodad).getName().getString();
+            // isTile = false;
+        } else {
+            tgtStr = "onto the ground";
+            // isTile = true;
+        }
+
+        if (invChanged) {
+            invFreeItems = player.inventory.containedItems.filter(it => (!it.isEquipped() && !it.isProtected())); // Exclude protected and equipped.
+            invTypes = invFreeItems.map(it => it.type);
+            invTypesUnique = invTypes.filter((ty, i) => invTypes.indexOf(ty) === i);
+        }
+        invChanged = false;
+
+        const cTypes = c.containedItems.map(it => it.type);
+        const cTypesUniqueMatched = cTypes.filter((ty, i) => cTypes.indexOf(ty) === i)
+            .filter(ty => invTypesUnique.includes(ty));
+
+        cTypesUniqueMatched.forEach((type) => {
+            const nHave = invFreeItems.filter(it => it.type == type).length;
+            const itMoved = IMgr.moveAllFromContainerToContainer(player, player.inventory, c, type, undefined, true);
+            const nMoved = itMoved.length
+            const itemStr = IMgr.getItemTypeGroupName(type, false, nMoved > 0 ? nMoved : 2).getString();
+
+            haveMatched = true;
+            if (!nMoved) {
+                haveMoved = true;
+                player.messages.send(SmartStack.INSTANCE["messageStackedNone"], itemStr + " " + tgtStr);
+            } else {
+                invChanged = true;
+                if (nMoved < nHave) player.messages.send(SmartStack.INSTANCE["messageStackedSome"], nMoved, nHave, itemStr + " " + tgtStr);
+                else player.messages.send(SmartStack.INSTANCE["messageStackedAll"], nMoved, itemStr + " " + tgtStr);
+            }
+        });
+    });
+    if(!haveMatched) player.messages.send(SmartStack.INSTANCE.messageNoMatch);
+    else if(haveMoved) {
+        player.updateStatsAndAttributes();
+        player.passTurn(TurnType.Idle);
+    }
+
+}
 
 export const StackNearby = new Action()
     .setUsableBy(EntityType.Player)
-    .setHandler((action) => {
-        const player = action.executor;
-        const IM = player.island.items;
-        const validContainerTypes = [ContainerReferenceType.Doodad, ContainerReferenceType.Tile];
-        const containers = IM.getAdjacentContainers(player, false)
-            .filter((c) => validContainerTypes.includes(IM.getContainerReference(c, undefined).crt))
-            .filter((c) => c.containedItems.length > 0);
+    .setHandler(handleStackNearby);
 
-        const invFreeItems = player.inventory.containedItems.filter(it => (!it.isEquipped() && !it.isProtected())); // Exclude protected and equipped.
-        const invTypes = invFreeItems.map(it => it.type);
-        const invTypesUnique = invTypes.filter((ty, i) => invTypes.indexOf(ty) === i);
-
-        let haveMatched = false;
-        if(containers.length) { 
-            SmartStack.LOG.info(`Sending message 'messageSearch'...`);
-            player.messages.send(Registry<SmartStack>().get("messageSearch"));
-        }
-        containers.forEach((c) => {
-            let cRef = IM.getContainerReference(c, undefined);
-            let tgtStr: string;
-
-            if (cRef.crt == ContainerReferenceType.Doodad) {
-                tgtStr = "into " + (c as Doodad).getName().getString();
-            } else {
-                tgtStr = "onto the ground";
-            }
-
-            let cTypes = c.containedItems.map(it => it.type);
-            let cTypesUniqueMatched = cTypes.filter((ty, i) => cTypes.indexOf(ty) === i)
-                .filter(ty => invTypesUnique.includes(ty));
-
-            cTypesUniqueMatched.forEach((type) => {
-                let nHave = player.inventory.containedItems.filter(it => it.type == type).length;
-                let nMoved = IM.moveAllFromContainerToContainer(player, player.inventory, c, type, undefined, true).length;
-                let itemStr = `${nMoved} ` + IM.getItemTypeGroupName(type, false, nMoved > 0 ? nMoved : 5).getString();
-                haveMatched = true;
-                let msgKey:keyof SmartStack;
-                if(nMoved == nHave) msgKey = "messageStackedAll";
-                else if(nMoved > 0) msgKey = "messageStackedSome";
-                else msgKey = "messageStackedNone";
-                SmartStack.LOG.info(`Sending message '${msgKey}'...`);
-                player.messages.send(Registry<SmartStack>().get(msgKey), itemStr, tgtStr);
-            });
-        });
-        if(!haveMatched) player.messages.send(Registry<SmartStack>().get("messageNoMatch"));
-    });
